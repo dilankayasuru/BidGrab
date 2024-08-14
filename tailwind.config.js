2887a1a36feb5f3a258e7a004a6acb41c4c10fee
dilankayasuru <dilankaysuru001@gmail.com>
2024-08-14 19:40:33 +0530

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php", "./lang/*.php", "./lang/*.html", "./js/custom.min.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
