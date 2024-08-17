fc171c9a11327ca63c4d5baf36f6393cc9516948
dilankayasuru <dilankaysuru001@gmail.com>
2024-08-17 22:04:00 +0530

    add comments

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <div class="logo">
-        <a href="#home">BidGarb</a>
+        <a href="home.php">BidGarb</a>
     </div>
     <ul>
         <li><a href="home.php" class="nav-links">Home</a></li>
@@ -12,17 +12,22 @@
 </nav>
 
 <script>
+    // Get the current URL
     const url = new URL(window.location.href);
+    // Extract the current page name from the URL
     const currentLocation = url.pathname.split('/').pop().split('.')[0];
+    // Select all navigation links
     const navLinks = document.querySelectorAll('.nav-links');
 
+    // Loop through each navigation link
     navLinks.forEach(link => {
+        // Check if the link text matches the current page name
         if (link.innerText.toLowerCase() === currentLocation.toLowerCase()) {
+            // Add 'active' class to the matching link
             link.classList.add('active')
-        }
-        else {
+        } else {
+            // Remove 'active' class from non-matching links
             link.classList.remove('active')
         }
     })
-
 </script>
