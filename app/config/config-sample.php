db48a170e41da63f3d819919ba030d02bd0bdd68
DilankaYasuru <dilankayasuru001@gmail.com>
2024-09-17 12:30:41 +0530

    remove config file

--- app/config/config-sample.php
+++ app/config/config-sample.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB_HOST','localhost');
-define('DB_USER', 'dilanka');
-define('DB_PASS', '3BWhLz.H1J1Z1j]A');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_NAME', 'bidgrab');
-define('DB_PORT', '3306');
+define('DB_PORT', '');
 define('BASE_URL', 'http://localhost/bidgrab/public/');
 
 ?>
